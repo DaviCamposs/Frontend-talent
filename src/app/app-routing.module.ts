342ac47c8eaf3f203d666f8827ff4528f6bcdb9b
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 15:57:10 -0300

    feat: add route pokemon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticatedGuard } from './core/guards/authenticated.guard';
+import { MainPagePokemonComponent } from './modules/pokemon/pages/main-page-pokemon/main-page-pokemon.component';
 
 const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
-  }
+  },
+  {
+    path: 'pokemon',
+    component: MainPagePokemonComponent,
+    canActivate: [AuthenticatedGuard]
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { PokemonModule } from './modules/pokemon/pokemon.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    PokemonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
