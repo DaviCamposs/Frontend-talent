cc387541515804ded320d01d762f692ea429f654
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 09:24:33 -0300

    feat: register method

--- src/app/modules/authentication/pages/register/register.component.ts
+++ src/app/modules/authentication/pages/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisteredEmailError } from 'src/app/core/errors/registeredEmailError';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-register',
@@ -11,18 +14,18 @@ export class RegisterComponent {
   registerForm: FormGroup
   formSubmit: boolean = false
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService, private notificationService: NotificationService) {
     this.registerForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
-    },{
-      validators: this.validatorEqualFields('password','confirmPassword')
+    }, {
+      validators: this.validatorEqualFields('password', 'confirmPassword')
     })
   }
 
-  validatorEqualFields(field1: string , field2: string) {
+  validatorEqualFields(field1: string, field2: string) {
     return (formGroup: FormGroup) => {
       const field1Control = formGroup.get(field1)
       const field2Control = formGroup.get(field2)
@@ -37,10 +40,25 @@ export class RegisterComponent {
 
   register() {
     this.formSubmit = true
-    if (!this.registerForm.valid) 
+    if (!this.registerForm.valid)
       return
+
+    const name = this.registerForm.get('name')?.value
+    const email = this.registerForm.get('email')?.value
+    const password = this.registerForm.get('password')?.value
+
+    this.authenticationService.register(name, email, password).subscribe(
+      () => {
+        this.notificationService.showMessageSuccess('Account registered!')
+        this.registerForm.reset()
+      },
+      error => {
+        let message = 'Error server, try again'
+        if (error instanceof RegisteredEmailError)
+          message = 'E-mail already registered'
+        this.notificationService.showMessageError(message)
+      }
+    )
   }
-
-
 
 }
