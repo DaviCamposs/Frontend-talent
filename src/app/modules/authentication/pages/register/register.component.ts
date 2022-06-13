eafde71e9e3db097bc62713ad59bdf0a30bd8194
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 08:29:43 -0300

    feat: not validate a form if password and confirmPassord are differente

--- src/app/modules/authentication/pages/register/register.component.ts
+++ src/app/modules/authentication/pages/register/register.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent {
 
   registerForm: FormGroup
+  formSubmit: boolean = false
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
@@ -16,7 +17,22 @@ export class RegisterComponent {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
       confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    },{
+      validators: this.validatorEqualFields('password','confirmPassword')
     })
+  }
+
+  validatorEqualFields(field1: string , field2: string) {
+    return (formGroup: FormGroup) => {
+      const field1Control = formGroup.get(field1)
+      const field2Control = formGroup.get(field2)
+
+      if (field1Control?.value === field2Control?.value) {
+        field2Control?.setErrors(null)
+      } else {
+        field2Control?.setErrors({ noEqualToPassword: true })
+      }
+    }
   }
 
 
