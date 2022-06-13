97e8efc0a0c749b4b10b42a96b9f7fdbbca4cb0b
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 16:12:08 -0300

    feat: add link to logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './core/services/authentication.service';
 
 @Component({
   selector: 'app-root',
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLogged: boolean = false
+
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.authenticationService.isLogged.subscribe(
+      (res) => {
+        this.isLogged = res
+      }
+    )
+  }
+
+  logout() {
+    this.authenticationService.logout()
   }
 
 }
