302b070e1c1c81200e5b061c2619228ec4511057
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 21:07:19 -0300

    feat: make a post Request in register

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { RegisterUserDTO } from './DTO/registerUserDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private base_url: string
+
+  constructor(private http: HttpClient) { 
+    this.base_url = environment.backendUrl
+  }
+
+  register(name: string , email: string , password: string): Observable<RegisterUserDTO> {
+    return this.http.post<RegisterUserDTO>(this.base_url + 'auth/login', { name , email , password })
+  }
 }
