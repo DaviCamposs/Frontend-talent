125d34463b009b1a199fb34900972724402aee3a
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 15:30:27 -0300

    feat: loginForm

--- src/app/modules/authentication/pages/login/login.component.ts
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  loginForm: FormGroup
+  submitForm: boolean = false
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { 
+    this.loginForm = this.fb.group({
+      email: [ '', [Validators.required,Validators.email] ],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
   }
+
 
 }
