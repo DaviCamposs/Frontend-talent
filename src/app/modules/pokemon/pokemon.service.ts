6c461faa54dfe0d6fb68dbe0c8845d6cf0f976bc
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 16:32:47 -0300

    feat: method to search Pokemon

--- src/app/modules/pokemon/pokemon.service.ts
+++ src/app/modules/pokemon/pokemon.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { PokemonSearchDTO } from './DTO/PokemonSearchDTO';
+import { PokemonNotFoundError } from './errors/pokemonNotFoundError';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  private base_url: string = 'https://pokeapi.co/api/v2/pokemon/'
+
+  constructor(private http: HttpClient) { }
+
+  searchPokemon(name: string): Observable<PokemonSearchDTO> {
+    return this.http.get<PokemonSearchDTO>(this.base_url+ name)
+      .pipe(
+        catchError(this.handlerError)
+      )
+  }
+
+  handlerError(error: HttpErrorResponse) {
+    return throwError(() => new PokemonNotFoundError());
+  }
 }
