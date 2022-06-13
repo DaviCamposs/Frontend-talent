8089cc3452a4abd98a85b6fb098475e7d335c187
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 21:28:42 -0300

    feat: RegisteredEmailMockResponse

--- src/app/core/__mocks__/registerUserMocks.ts
+++ src/app/core/__mocks__/registerUserMocks.ts
@@ -2,3 +2,8 @@ export const SuccessRegisterUserMockResponse = {
     success: true,
     message: 'User registered successfully'
 }
+
+export const RegisteredEmailMockResponse = {
+    title: 'Validation Error',
+    message: 'E-mail already registered'
+}
