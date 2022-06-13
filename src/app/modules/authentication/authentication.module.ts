cc530c89dad3eee551bd23f41691a5f681cb5f57
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 09:58:27 -0300

    feat: loginComponent

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { RegisterComponent } from './pages/register/register.component';
 import { CoreModule } from 'src/app/core/core.module';
+import { LoginComponent } from './pages/login/login.component';
 
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
