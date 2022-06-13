f913d853bf180bbbec5611fbaa3efa9fa644f9fa
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 15:36:23 -0300

    feat: login user

--- src/app/modules/authentication/pages/login/login.component.ts
+++ src/app/modules/authentication/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { WrongCredentialsError } from 'src/app/core/errors/wrongCredentialsError';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { NotificationService } from 'src/app/core/services/notification.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,7 @@ export class LoginComponent {
   loginForm: FormGroup
   submitForm: boolean = false
 
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder, private authenticationService: AuthenticationService, private notificationService: NotificationService) { 
     this.loginForm = this.fb.group({
       email: [ '', [Validators.required,Validators.email] ],
       password: ['', [Validators.required, Validators.minLength(8)]]
@@ -34,6 +37,21 @@ export class LoginComponent {
     if (this.loginForm.invalid) {
       return
     }
+
+    const email = this.loginForm.get('email')?.value
+    const password = this.loginForm.get('password')?.value
+
+    this.authenticationService.login(email, password).subscribe(
+      () => {
+        // redirect
+      },
+      error => {
+        let message = 'Error server, try again'
+        if (error instanceof WrongCredentialsError)
+          message = 'Verify your email and password'
+        this.notificationService.showMessageError(message)
+      }
+    )
   }
 
 
