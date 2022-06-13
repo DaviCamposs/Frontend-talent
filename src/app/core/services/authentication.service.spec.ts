cdbc6d5bac7e6d6059fb76f7fd9154e5760c50b5
DaviCamposs <davi.campos689@gmail.com>
2022-06-12 21:07:24 -0300

    test: make a post Request in register

--- src/app/core/services/authentication.service.spec.ts
+++ src/app/core/services/authentication.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing'
 
 import { AuthenticationService } from './authentication.service';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
+  let httpClient: HttpTestingController
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AuthenticationService]
+    });
+    httpClient = TestBed.inject(HttpTestingController)
     service = TestBed.inject(AuthenticationService);
   });
+
+  afterEach(() => {
+    httpClient.verify()
+  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should make a POST request in register', (done: DoneFn) => {
+    const url = 'http://localhost:3000/api/v1/auth/login'
+
+    service.register('david','david@mail.com','12345678').subscribe(() => {
+      done()
+    })
+
+    const req: TestRequest = httpClient.expectOne(url)
+
+    expect(req.request.urlWithParams).toEqual(url);
+    expect(req.request.method).toEqual('POST');
+
+    req.flush({});
+  })
 });
