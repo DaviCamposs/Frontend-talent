770c2a6d5f499efd1527dc10c6776fff6de0266a
DaviCamposs <davi.campos689@gmail.com>
2022-06-13 08:10:35 -0300

    feat: registerForm

--- src/app/modules/authentication/pages/register/register.component.ts
+++ src/app/modules/authentication/pages/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  registerForm: FormGroup
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    })
   }
+
+
 
 }
